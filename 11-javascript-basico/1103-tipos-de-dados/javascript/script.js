116b3b045cf834f2aa4c59b626fa0582a577dae5
LucasSD00 <lk7750785@outlook.com>
2023-07-15 21:52:56 -0300

    1103 Tipos de Dados

---
+++ 11-javascript-basico/1103-tipos-de-dados/javascript/script.js
@@ -0,0 +1,17 @@
+let total = 0;
+
+const compras = 30;
+const imposto = 10;
+
+total = compras + imposto;
+
+const dobro = total * 2;
+const dividir = total / 2;
+const desconto = total - 30;
+
+const strings = "20" + "20";
+const numbers = 20 + 20;
+
+const transfomar = Number('20')
+
+console.log(numbers);
